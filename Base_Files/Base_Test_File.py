78c05fe7028f9b26937cc2874908b4e1eaf3f781
bhargav-baleja <bhargav.baleja.11@gmail.com>
2021-03-05 11:44:58 -0500

    Added a new file Base_Test_File

---
+++ Base_Files/Base_Test_File.py
@@ -0,0 +1,20 @@
+""" File that contains all the test cases. """
+import unittest
+from typing import List
+import datetime
+
+from Base_Files.Repository import Repository
+
+class TestRepository(unittest.TestCase):
+    """ Class that contains all the test cases. """
+
+    def __init__(self, *args, **kwargs):
+        """ Function that initializes class variable repository. """
+        super(TestRepository, self).__init__(*args, **kwargs)
+        # Creating an object of class Repository that will contains both individual and family dictionaries.
+        # Pass the path of your GEDCOM file as a parameter below.
+        self.repository = Repository('../GedcomFiles/ssw555_input_file.ged')
+
+if __name__ == "__main__":
+    """ Runs all the tests created above. """
+    unittest.main(exit=False, verbosity=2)
