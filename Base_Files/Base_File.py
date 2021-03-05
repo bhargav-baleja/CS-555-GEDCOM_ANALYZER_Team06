bdbd147516199058a9e98e69217f095638f23df8
bhargav-baleja <bhargav.baleja.11@gmail.com>
2021-03-05 11:40:40 -0500

    Added a new files family, Base_File and Logger.

---
+++ Base_Files/Base_File.py
@@ -0,0 +1,24 @@
+""" File that runs all the user stories and prints their results. """
+import sys
+
+from Base_Files.Logger import Logger
+from Base_Files.Repository import Repository
+
+
+def main():
+    """ Function that runs all the user stories and prints their results. """
+    sys.stdout = Logger()
+    # Creating an object of class Repository that will contains both individual and family dictionaries.
+    # Pass the path of your GEDCOM file as a parameter below.
+    repository = Repository("../GedcomFiles/ssw555_input_file.ged")
+    individual = repository.get_individual()
+    family = repository.get_family()
+
+    # Prints individual pretty table.
+    repository.individual_pretty_table()
+    # Prints family pretty table.
+    repository.family_pretty_table()
+
+if __name__ == '__main__':
+    """ Calls main method. """
+    main()
