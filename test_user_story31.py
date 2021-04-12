38b04ff0892c8a44773fe1c506131f1752f2be8d
Ananyarajendra <ananyar126@gmail.com>
2021-04-12 11:06:28 -0400

    Add files via upload

---
+++ test_user_story31.py
@@ -0,0 +1,100 @@
+import unittest
+from US_31 import *
+
+
+class TestUserStory31(unittest.TestCase):
+    def setUp(self):
+        ind1 = Individual("I1")
+        ind1.name = "Single /Living1/"
+        ind1.birth = datetime.strptime("20 AUG 1998", "%d %b %Y").date()
+        ind2 = Individual("I2")
+        ind2.name = "Single /Living2/"
+        ind2.birth = datetime.strptime("20 AUG 1998", "%d %b %Y").date()
+        ind3 = Individual("I3")
+        ind3.name = "Single /Living3/"
+        ind3.birth = datetime.strptime("20 AUG 1998", "%d %b %Y").date()
+        ind4 = Individual("I4")
+        ind4.name = "Single /Living4/"
+        ind4.birth = datetime.strptime("20 AUG 1998", "%d %b %Y").date()
+
+        individuals.append(ind1)
+        individuals.append(ind2)
+        individuals.append(ind3)
+        individuals.append(ind4)
+        families.append(Family("F1"))
+
+    def tearDown(self):
+        individuals.clear()
+        families.clear()
+
+    def test_empty_file(self):
+        individuals.clear()
+        families.clear()
+        table = []
+        list_living_single(table)
+        self.assertFalse(table[0][4].split()) and self.assertTrue(table[0][3])
+
+    def test_married_and_deceased(self):
+        get_individual("I2").name = "Married /Living2/"
+        get_individual("I2").spouse_id = "F1"
+        get_individual("I3").name = "Married /Deceased3/"
+        get_individual("I3").spouse_id = "F1"
+        get_individual("I3").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        get_individual("I4").name = "Single /Deceased4/"
+        get_individual("I4").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        table = []
+        list_living_single(table)
+        self.assertCountEqual([get_individual("I1").name], table[0][4].split("\n")) and self.assertTrue(table[0][3])
+
+    def test_some_married_and_all_deceased(self):
+        get_individual("I1").name = "Single /Deceased1/"
+        get_individual("I1").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        get_individual("I2").name = "Married /Deceased2/"
+        get_individual("I2").spouse_id = "F1"
+        get_individual("I2").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        get_individual("I3").name = "Married /Deceased3/"
+        get_individual("I3").spouse_id = "F1"
+        get_individual("I3").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        get_individual("I4").name = "Single /Deceased4/"
+        get_individual("I4").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        table = []
+        list_living_single(table)
+        self.assertFalse(table[0][4].split()) and self.assertTrue(table[0][3])
+
+    def test_some_married_and_no_deceased(self):
+        get_individual("I2").name = "Married /Living2/"
+        get_individual("I2").spouse_id = "F1"
+        get_individual("I3").name = "Married /Living3/"
+        get_individual("I3").spouse_id = "F1"
+        table = []
+        list_living_single(table)
+        self.assertCountEqual([get_individual("I1").name, get_individual("I4").name],
+                              table[0][4].split("\n")) and self.assertTrue(table[0][3])
+
+    def test_all_married_and_some_deceased(self):
+        families.append(Family("F2"))
+        get_individual("I1").name = "Married /Deceased1/"
+        get_individual("I1").spouse_id = "F2"
+        get_individual("I1").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        get_individual("I4").name = "Married /Deceased4/"
+        get_individual("I4").spouse_id = "F2"
+        get_individual("I4").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        get_individual("I2").spouse_id = "F1"
+        get_individual("I3").spouse_id = "F1"
+        table = []
+        list_living_single(table)
+        self.assertFalse(table[0][4].split()) and self.assertTrue(table[0][3])
+
+    def test_no_married_and_some_deceased(self):
+        get_individual("I2").name = "Single /Deceased2/"
+        get_individual("I2").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        get_individual("I3").name = "Single /Deceased3/"
+        get_individual("I3").death = datetime.strptime("20 AUG 2000", "%d %b %Y").date()
+        table = []
+        list_living_single(table)
+        self.assertCountEqual([get_individual("I1").name, get_individual("I4").name],
+                              table[0][4].split("\n")) and self.assertTrue(table[0][3])
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
